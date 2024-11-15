f09baa128fec43625ab758246088d02bbd805b83
blastorp <jernestm@gmail.com>
2024-11-15 10:10:19 -0600

    barra nav, global css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+
 import './App.css';
+import MyNavBar from './layouts/navegacionAdmin/navbar';
+import { useState } from 'react';
 
 function App() {
+
+
+  const arrayLinks = [
+    {nombre: 'Mi Dashboard' , ruta:'#'}, 
+    {nombre: 'Vista Cliente' , ruta:'#'}
+
+  ]
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <MyNavBar listaLinks={arrayLinks} />
     </div>
   );
 }
---
+++ src/layouts/navegacionAdmin/navbar.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import '../navegacionAdmin/estilos/navbar.css'
+import { useState } from 'react';
+
+function MyNavBar({listaLinks}) {
+    const [isSidebarVisible, setIsSidebarVisible] = useState(false);
+
+    const showSidebar = () => setIsSidebarVisible(true);
+    const hideSidebar = () => setIsSidebarVisible(false);
+
+    return (
+        <nav>
+            {/* Sidebar */}
+            <ul className={`sidebar ${isSidebarVisible ? "visible" : ""}`}>
+                <li onClick={hideSidebar}>
+                    <a href="#">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            height="26"
+                            viewBox="0 96 960 960"
+                            width="26"
+                        >
+                            <path d="m249 849-42-42 231-231-231-231 42-42 231 231 231-231 42 42-231 231 231 231-42 42-231-231-231 231Z" />
+                        </svg>
+                    </a>
+                </li>
+                {listaLinks.map((item,index) => (
+                    <li><a href={item.ruta}>{item.nombre}</a></li>
+                ))}
+                {/* <li><a href="#">Blog</a></li>
+                <li><a href="#">Products</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Forum</a></li>
+                <li><a href="#">Login</a></li> */}
+            </ul>
+
+            {/* Main Navigation */}
+            <ul>
+                <li className="titulo"><a  href="#">Temple of Ink</a></li>
+                {listaLinks.map((item,index) => (
+                    <li className="hideOnMobile"><a href={item.ruta}>{item.nombre}</a></li>
+                ))}
+
+
+                {/* <li className="hideOnMobile"><a href="#">Blog</a></li>
+                <li className="hideOnMobile"><a href="#">Products</a></li>
+                <li className="hideOnMobile"><a href="#">About</a></li>
+                <li className="hideOnMobile"><a href="#">Forum</a></li>
+                <li className="hideOnMobile"><a href="#">Login</a></li> */}
+                <li className="menu-button" onClick={showSidebar}>
+                    <a href="#">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            height="26"
+                            viewBox="0 96 960 960"
+                            width="26"
+                        >
+                            <path d="M120 816v-60h720v60H120Zm0-210v-60h720v60H120Zm0-210v-60h720v60H120Z" />
+                        </svg>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    );
+}
+
+
+export default MyNavBar;
