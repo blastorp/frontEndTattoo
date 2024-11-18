7e9972db2dbf52b9919cd576d37d033e48186fa7
blastorp <jernestm@gmail.com>
2024-11-18 09:27:31 -0600

    ajustes estilos 2

--- src/modulos1/Artistas/pages/artistasDash.jsx
+++ src/modulos1/Artistas/pages/artistasDash.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import MainAdmin from '../../../layouts/MainAdmin'
 import TarjetaLinkAdd from '../components/TarjetaLInkAdd'
+
+interface Post {
+  id: Number;
+  title: String;
+}
 function ArtistasDash() {
   return (
     <MainAdmin tituloPagina={"Administracion Perfiles de Artistas"}>
