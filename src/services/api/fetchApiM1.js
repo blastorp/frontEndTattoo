3f31cedc534f677478243cc9a7a9569484977f35
CristianBrenesV <brenescris1@gmail.com>
2024-11-27 14:05:20 -0600

    fecth Cristian

--- src/services/api/fetchApiM1.js
+++ src/services/api/fetchApiM1.js
@@ -1,8 +1,12 @@
-const BASE_URL = "https://tiusr39pl.cuc-carrera-ti.ac.cr/apiTattoo";
+const BASE_URL_1 = "https://tiusr39pl.cuc-carrera-ti.ac.cr/apiTattoo";
+const BASE_URL_2 = "http://localhost:5008/";
 
+// fetchApiM1 Jhon
 const fetchApiM1 = async (segmentoRuta, cabecera) => {
   try {
-    const response = await fetch(`${BASE_URL}/${segmentoRuta}`);
+    const response = await fetch(`${BASE_URL_1}/${segmentoRuta}`, {
+      headers: cabecera,
+    });
     if (!response.ok) {
       throw new Error("Failed to fetch data");
     }
@@ -10,7 +14,23 @@ const fetchApiM1 = async (segmentoRuta, cabecera) => {
   } catch (error) {
     console.error("Fetch error:", error);
     throw error;
-  }  
+  }
 };
 
-export default fetchApiM1
+//fetchApiM2 Cristian
+const fetchApiM2 = async (segmentoRuta, cabecera) => {
+  try {
+    const response = await fetch(`${BASE_URL_2}/${segmentoRuta}`, {
+      headers: cabecera,
+    });
+    if (!response.ok) {
+      throw new Error("Failed to fetch data");
+    }
+    return await response.json();
+  } catch (error) {
+    console.error("Fetch error:", error);
+    throw error;
+  }
+};
+
+export { fetchApiM1, fetchApiM2 };
